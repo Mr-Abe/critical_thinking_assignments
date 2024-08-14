f181aeb9be3c9b6f328b84d6bc99ccb93a5e55dd
mrabe <119377891+Mr-Abe@users.noreply.github.com>
2024-08-14 16:11:42 -0400

    Breaking down assignment 3 part 1

--- Module_3_Assignment/module_3_assignment_part_1.py
+++ Module_3_Assignment/module_3_assignment_part_1.py
@@ -5,4 +5,13 @@ Write a program that calculates the total amount of a meal purchased at a restau
 The program should ask the user to enter the charge for the food and then calculate 
 the amounts with an 18 percent tip and 7 percent sales tax. 
 Display each of these amounts and the total price.
+
+U - given int price find the tip and the tax. tip is 18 percent of the charge. Tax is 7 percent. 
+    Both calculated on the charge itself. Return each individual and combined as a total. Output should look nice.
+M - create a bill class and provide methods for calculating tip, tax, as well as providing output.
+P
+I
+R
+E
 '''
+
