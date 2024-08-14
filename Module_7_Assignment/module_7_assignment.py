b74c9b1f24ac4491fb74daa9479fb2d97abf04e5
mrabrahams <119377891+Mr-Abe@users.noreply.github.com>
2024-08-14 07:42:09 -0400

    Added instructions for later assignments.

---
+++ Module_7_Assignment/module_7_assignment.py
@@ -0,0 +1,29 @@
+"""
+This program stores course information (room number, instructor, meeting time) in dictionaries.
+
+The program should let the user enter a course number and then it should display 
+the course's room number, instructor, and meeting time.
+
+Key-value pairs for the dictionaries are as follows:
+
+Course-Room Dictionary:
+- 'CSC101': 3004
+- 'CSC102': 4501
+- 'CSC103': 6755
+- 'NET110': 1244
+- 'COM241': 1411
+
+Course-Instructor Dictionary:
+- 'CSC101': 'Haynes'
+- 'CSC102': 'Alvarado'
+- 'CSC103': 'Rich'
+- 'NET110': 'Burke'
+- 'COM241': 'Lee'
+
+Course-Meeting Time Dictionary:
+- 'CSC101': '8:00 a.m.'
+- 'CSC102': '9:00 a.m.'
+- 'CSC103': '10:00 a.m.'
+- 'NET110': '11:00 a.m.'
+- 'COM241': '1:00 p.m.'
+"""
