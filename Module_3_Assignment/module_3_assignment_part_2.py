68b00535e75b01d5446fae0ee2e613cef726d49b
mrabrahams <119377891+Mr-Abe@users.noreply.github.com>
2024-08-31 19:19:51 -0400

    Completed part II with testing.

--- Module_3_Assignment/module_3_assignment_part_2.py
+++ Module_3_Assignment/module_3_assignment_part_2.py
@@ -8,30 +8,64 @@ the time now (in hours) and then ask for the number of hours to wait for the ala
 Your program should output what the time will be on a 24-hour clock when the alarm goes off.
 '''
 
-def getInput():
+def getInputHoursOnly():
+    curr_hours = int(input('What is the current time now in hours (0-23)? '))
     
-    curr_time = int(input('What is the time now in hours (0-23)? '))
-
-    while curr_time < 0 or curr_time > 23:
-        print('Please enter a valid time between 0 and 23.')
-        curr_time = int(input('What is the time now in hours (0-23)? '))
+    while curr_hours < 0 or curr_hours > 23:
+        print("Please enter a valid time between 0 and 23.")
+        curr_hours = int(input('What is the current time now in hours (0-23)? '))
 
     hours_till_alarm = int(input('Please provide the number of hours to wait until the alarm: '))
+
+    return curr_hours, hours_till_alarm
+
+def getInputHoursAndMinutes():
+    curr_hours = int(input('What is the current time now in hours (0-23)? '))
     
-    return curr_time, hours_till_alarm
+    while curr_hours < 0 or curr_hours > 23:
+        print("Please enter a valid time between 0 and 23.")
+        curr_hours = int(input('What is the current time now in hours (0-23)? '))
 
-def convert_2_24(curr_time, hours_till_alarm):
+    curr_minutes = int(input('What are the current minutes (0-59)? '))
+    
+    while curr_minutes < 0 or curr_minutes > 59:
+        print("Please enter a valid number of minutes between 0 and 59.")
+        curr_minutes = int(input('What are the current minutes (0-59)? '))
 
-    alarm_time = (curr_time + hours_till_alarm) % 24
+    hours_till_alarm = int(input('Please provide the number of hours to wait until the alarm: '))
+    minutes_till_alarm = int(input('Please provide the number of minutes to wait until the alarm: '))
 
+    return curr_hours, curr_minutes, hours_till_alarm, minutes_till_alarm
+
+def convert_2_24_hours_only(curr_hours, hours_till_alarm):
+    alarm_time = (curr_hours + hours_till_alarm) % 24
     return alarm_time
 
+def convert_2_24_hours_and_minutes(curr_hours, curr_minutes, hours_till_alarm, minutes_till_alarm):
+    total_minutes = curr_minutes + minutes_till_alarm
+    additional_hours = total_minutes // 60
+    final_minutes = total_minutes % 60
+    
+    total_hours = (curr_hours + hours_till_alarm + additional_hours) % 24
+
+    return total_hours, final_minutes
 
 if __name__ == '__main__':
-    print('''Welcome to scharmy alarmy, where you can provide the time now and hours until alert. 
-          The program will then give you the time of the alarm using a 24 hour clock.''')
-    
-    curr_time, hours_till_alarm = getInput()
+    print('''Welcome to Scharmy Alarmy! Please choose an option:
+    1. Use hours only
+    2. Use hours and minutes''')
 
-    alarm_time = convert_2_24(curr_time, hours_till_alarm)
-    print(f'The alarm will go off at {alarm_time}:00 hours.')
+    choice = input('Enter 1 or 2: ')
+
+    if choice == '1':
+        curr_hours, hours_till_alarm = getInputHoursOnly()
+        alarm_hours = convert_2_24_hours_only(curr_hours, hours_till_alarm)
+        print(f'The alarm will go off at {alarm_hours:02}:00 hours.')
+
+    elif choice == '2':
+        curr_hours, curr_minutes, hours_till_alarm, minutes_till_alarm = getInputHoursAndMinutes()
+        alarm_hours, alarm_minutes = convert_2_24_hours_and_minutes(curr_hours, curr_minutes, hours_till_alarm, minutes_till_alarm)
+        print(f'The alarm will go off at {alarm_hours:02}:{alarm_minutes:02} hours.')
+    
+    else:
+        print('Invalid choice. Please restart the program and choose either 1 or 2.')
