2f07f911b590589c780dbbd7af0adc5ce0333e90
mrabrahams <119377891+Mr-Abe@users.noreply.github.com>
2024-08-31 13:14:21 -0400

    Alarm calculation complete.

--- Module_3_Assignment/module_3_assignment_part_2.py
+++ Module_3_Assignment/module_3_assignment_part_2.py
@@ -7,3 +7,31 @@ Write a Python program to solve the general version of the above problem. Ask th
 the time now (in hours) and then ask for the number of hours to wait for the alarm. 
 Your program should output what the time will be on a 24-hour clock when the alarm goes off.
 '''
+
+def getInput():
+    
+    curr_time = int(input('What is the time now in hours (0-23)? '))
+
+    while curr_time < 0 or curr_time > 23:
+        print('Please enter a valid time between 0 and 23.')
+        curr_time = int(input('What is the time now in hours (0-23)? '))
+
+    hours_till_alarm = int(input('Please provide the number of hours to wait until the alarm: '))
+    
+    return curr_time, hours_till_alarm
+
+def convert_2_24(curr_time, hours_till_alarm):
+
+    alarm_time = (curr_time + hours_till_alarm) % 24
+
+    return alarm_time
+
+
+if __name__ == '__main__':
+    print('''Welcome to scharmy alarmy, where you can provide the time now and hours until alert. 
+          The program will then give you the time of the alarm using a 24 hour clock.''')
+    
+    curr_time, hours_till_alarm = getInput()
+
+    alarm_time = convert_2_24(curr_time, hours_till_alarm)
+    print(f'The alarm will go off at {alarm_time}:00 hours.')
